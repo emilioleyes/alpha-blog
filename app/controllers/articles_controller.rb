167bd1382964c504d9790b4e2647cd28438f616b
emilioleyes <emilioleyes@gmail.com>
2021-10-06 11:59:35 -0300

    Luego de agregar funcion de EDITy UPDATE

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,37 @@ def show
 end
 
 def index
-  @articles = Article.all 
+  @articles = Article.all
 end
 
+def new
+  @article = Article.new
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def create
+  @article = Article.new(params.require(:article).permit(:title, :description))
+
+  if @article.save
+    flash[:notice] = "Articulo se creó correctamente"
+    redirect_to @article
+  else
+    render 'new'
+  end
+
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(params.require(:article).permit(:title, :description))
+    flash[:notice] = "Articulo actualizado exitosamente"
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
 
 end
