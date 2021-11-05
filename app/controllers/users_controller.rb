d07f4e286bb60a7acd9ce73e8e5af294ed652a45
emilioleyes <emilioleyes@gmail.com>
2021-11-04 21:32:18 -0300

    agregado de paginación

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
@@ -17,7 +21,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "cuenta actualizada correctamente"
-      redirect_to articles_path
+      # redirect_to user_path(@user) --- alternativa larga ---
+      redirect_to @user
     else
       render 'edit'
     end
