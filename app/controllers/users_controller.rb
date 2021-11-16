c605616eb4d6b314eda7f2e0eca898c82ad5b245
emilioleyes <emilioleyes@gmail.com>
2021-11-16 19:39:26 -0300

    finalizacion funciones Admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "la cuenta y todos sus articulos se borraron"
     redirect_to articles_path
   end
@@ -57,7 +57,7 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
         flash[:alert] = "solo puedes editar o borrar tu propio perfil"
         redirect_to @user
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_122120) do
+ActiveRecord::Schema.define(version: 2021_11_16_220025) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_11_03_122120) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
