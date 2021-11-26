cf2e9eafdc4477d7854481a5d9f933e2a4c47912
emilioleyes <emilioleyes@gmail.com>
2021-11-26 18:25:18 -0300

    fin de alpha blog  - completo

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+before_action :require_admin, except: [:index, :show, :edit]
+
 def new
   @category = Category.new
 end
@@ -15,16 +17,40 @@ def create
 end
 
 def index
+  # @categories = Category.all
+  @categories = Category.paginate(page: params[:page], per_page: 5)
 
+end
+
+def edit
+  @category = Category.find(params[:id])
+end
+
+def update
+  @category = Category.find(params[:id])
+  if @category.update(category_params)
+    flash[:notice] = "Se actualizó correctamente la categoria"
+  else
+    render edit
+  end
+  
 end
 
 def show
   @category = Category.find(params[:id])
+  @articles = @category.articles.paginate(page: params[:page], per_page: 5)
 end
 
 private
 def category_params
   params.require(:category).permit(:name)
+end
+
+def require_admin
+  if !(logged_in? && current_user.admin?)
+    flash[:alert] = "Solo los administradores pueden realizar esto"
+    redirect_to categories_path
+  end
 end
 
 end
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin_user = User.create(username: "eleyes", email: "eleyes@sam.com.ar",
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test "obtener el nuevo formulario de categoría y crear categoría" do
     get "/categories/new"
     assert_response :success
@@ -12,4 +18,18 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "Sports", response.body
   end
+
+
+
+  test "obtener nuevo formulario de categoría y rechazar creación invalida" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " "} }
+    end
+    assert_match "errores", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+
 end
